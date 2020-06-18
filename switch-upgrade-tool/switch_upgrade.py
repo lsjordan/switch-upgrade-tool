5e0c6f6f9215238f5e6eac8b256501ecbb66e6b5
Liam Jordan <lsjordan.uk@gmail.com>
2020-06-18 13:03:50 +0100

    sudo code and notes

---
+++ switch-upgrade-tool/switch_upgrade.py
@@ -0,0 +1,9 @@
+# get and validate input
+#     hosts
+#     username
+#     password
+#     device type
+#     upgrade level (check, copy, upgrade, reload)
+
+# build a list of upgrade objects
+# start upgrade workers
---
+++ switch-upgrade-tool/upgrade.py
@@ -0,0 +1,18 @@
+# build switch object
+# start "status" variable
+# validate switch ssh
+# get upgrade type
+#     ios
+#     iosxe
+# set config file
+# set upgrade function
+# open config file
+# validate switch supported
+# compare versions
+# take backups
+# transfer file
+# execute upgrade function
+# reboot
+# wait for switch
+# check version
+# finish

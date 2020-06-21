b9a3b999813786f2e24595ed4c14b85ec1a53f6a
Liam Jordan <lsjordan.uk@gmail.com>
2020-06-21 18:15:34 +0100

    GUI work

--- switch-upgrade-tool/gui.py
+++ switch-upgrade-tool/gui.py
@@ -6,6 +6,8 @@ class GUI:
     """A class to handle the shell GUI"""
     def __init__(self, upgrades):
         self.upgrades = upgrades
+        # self.device_count = len(upgrades)
+        self.device_count = 10
 
     def get_status(self):
         """ a test"""
@@ -15,12 +17,16 @@ class GUI:
     def c_main(self, screen):
         """The main GUI window"""
         screen = curses.initscr()
-        screen.addstr(0, 0, "This string gets printed at position (0, 0)")
-        screen.addstr(
-            3, 1, "Try Russian text: Привет")  # Python 3 required for unicode
-        screen.addstr(4, 4, "X")
-        screen.addch(5, 5, "Y")
-        screen.refresh()
+        height, width = screen.getmaxyx()
+        header = curses.newwin(3, width, 0, 0)
+        header.addstr(1, 1, "Switch Upgrade tool running...")
+        header.addstr(2, 1, "Liam Jordan, 2020")
+        header.refresh()
+        device = curses.newwin(self.device_count, int(width / 2), 4, 0)
+        device.addstr(1, int(width / 4), "testing")
+        for x in range(0, self.device_count):
+            device.addstr(x, int(width / 4), "testing")
+        device.refresh()
         time.sleep(15)
 
     def start(self):

798043d0a4e2418a9adfa6201ba56caba87fe8b2
Liam Jordan <lsjordan.uk@gmail.com>
2020-06-21 11:38:53 +0100

    GUI template

--- switch-upgrade-tool/gui.py
+++ switch-upgrade-tool/gui.py
@@ -1,4 +1,6 @@
 import curses
+import time
+
 
 class GUI:
     """A class to handle the shell GUI"""
@@ -9,3 +11,18 @@ class GUI:
         """ a test"""
         for upgrade in self.upgrades:
             print(upgrade.status)
+
+    def c_main(self, screen):
+        """The main GUI window"""
+        screen = curses.initscr()
+        screen.addstr(0, 0, "This string gets printed at position (0, 0)")
+        screen.addstr(
+            3, 1, "Try Russian text: Привет")  # Python 3 required for unicode
+        screen.addstr(4, 4, "X")
+        screen.addch(5, 5, "Y")
+        screen.refresh()
+        time.sleep(15)
+
+    def start(self):
+        """Starts the GUI"""
+        curses.wrapper(self.c_main)
